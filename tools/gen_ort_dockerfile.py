11f4103d000fcda85f828968e446b834398ac1fb
Julien Cuquemelle <j.cuquemelle@criteo.com>
2026-03-26 10:33:35 +0100

    Parametrize max parallelism at docker level for ORT build

--- tools/gen_ort_dockerfile.py
+++ tools/gen_ort_dockerfile.py
@@ -380,7 +380,8 @@ RUN git clone -b rel-${ONNXRUNTIME_VERSION} --recursive ${ONNXRUNTIME_REPO} onnx
 
     df += """
 WORKDIR /workspace/onnxruntime
-ARG COMMON_BUILD_ARGS="--config ${{ONNXRUNTIME_BUILD_CONFIG}} --parallel --skip_submodule_sync --build_shared_lib \
+ARG PARALLEL_JOBS
+ARG COMMON_BUILD_ARGS="--config ${{ONNXRUNTIME_BUILD_CONFIG}} --parallel ${{PARALLEL_JOBS}} --skip_submodule_sync --build_shared_lib \
     --compile_no_warning_as_error --build_dir /workspace/build --cmake_extra_defines CMAKE_CUDA_ARCHITECTURES='{}'  --cmake_extra_defines CMAKE_POLICY_VERSION_MINIMUM=3.5 --build_wheel"
 """.format(
         cuda_archs
